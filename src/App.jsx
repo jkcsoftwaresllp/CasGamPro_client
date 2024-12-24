13ddfae3e8cb79145e50ed3a3733c780f4d8e843
Danishan2 <danishan@jksoftwares.com>
2024-12-24 10:18:55 +0530

    feat: Updating Project Setting for Vite + React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { CasGamPro } from "./components/CasGamPro";
+
+function App() {
+
+  return (
+    <div>
+      <CasGamPro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CasGamPro.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CasGamPro = () => {
+  return <>CasGamPro V-1.0.0</>;
+};
