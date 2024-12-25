1707b8a3cb54b6b697a0ad3648a5cf49a59d993f
Rishabh Mishra <rishabh.mishra@jkcsoftwares.com>
2024-12-25 21:35:47 +0530

    feat: Update button styles across components and add new user button in ManageClient

--- src/pages/AdminDashboard/ManageClient.jsx
+++ src/pages/AdminDashboard/ManageClient.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"; // Import useNavigate from React Router
+import style from "../../styles/ManageClient.module.css"; // Import CSS Module
 
 const ManageClients = () => {
   const navigate = useNavigate(); // Initialize navigate function
@@ -11,7 +12,9 @@ const ManageClients = () => {
     <div>
       <h2>Manage Clients</h2>
       {/* Button to create a new user */}
-      <button onClick={handleCreateUserClick}>Create New User</button>
+      <button className={style.newUserBtn} onClick={handleCreateUserClick}>
+        + New User
+      </button>
       {/* Add more content related to managing clients here */}
     </div>
   );
