75337f13271f922060e18f30698ed06a44109fd7
Salman-jkc <mohd.salman@jkcsoftwares.com>
2024-12-31 16:08:39 +0530

    Style: Update BetInRectangle and BetOnCard CSS styles

--- src/component/test/test.jsx
+++ src/component/test/test.jsx
@@ -2,13 +2,15 @@ import { BetOnCard } from "../game/commonGameComponent/BetSection/jsx/BetOnCard"
 
 const Test = () => {
   return (
-    // <div style={{backgroundColor: '#fff', padding: '10px'}}>
-    <div>
+    <div style={{ backgroundColor: "#fff", padding: "10px" }}>
+      {/* <div> */}
       <BetOnCard
         label={"A"}
         betAmount={"0.0"}
         betProfit={"1.75"}
-        onClick={() => {}}
+        onClick={(value) => {
+          console.log(value);
+        }}
         isLock={false}
       />
     </div>
