aa6c3ff26484021daf0436f45fa72693dd470a23
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-01-05 20:33:07 -0800

    Feat/salman (#33)
    * Feat: Added Admin Dashboard
    
    * test: Update text file
    
    * feat: Add AndarBaharBetSection and BetCardSection components with styles
    
    ---------
    
    Co-authored-by: ssalmannn <salman029sheikh@gmail.com>

---
+++ src/component/game/anderBahar/jsx/AndarBaharBetSection.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/BetSection.module.css";
+
+export const AndarBaharBetSection = ({}) => {
+  return <div className={styles.andarBaharBetSection}></div>;
+};
--- src/component/test/test.jsx
+++ src/component/test/test.jsx
@@ -1,10 +1,9 @@
-import { Card } from "../game/commonGameComponent/cardSection/jsx/Card";
+import { BetCardSection } from "../game/commonGameComponent/BetSection/jsx/BetCardSection";
 
 const Test = () => {
   return (
-    <div style={{ backgroundColor: "#fff", padding: "10px" }}>
-      {/* <div> */}
-      <Card code={"D10"} onClick={() => {}} />
+    <div>
+      <BetCardSection isLock={false}/>
     </div>
   );
 };
