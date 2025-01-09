e0cc2cc14cdad8c084c22dbcacb8a068560adfda
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-01-09 17:32:00 +0530

    feat: Refactor styles in StakeSection, MainBet, UserLabelListWindow, and UserBetMain for improved layout and responsiveness

--- src/component/game/common/placeBet/MainBet.jsx
+++ src/component/game/common/placeBet/MainBet.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import { BetComponent } from "./betBody/BetComponent";
 import { UserLabelHeader } from "./betBody/BetHeader";
 import { MainHeader } from "./betHeader/MainHeader";
-import styles from './MainBet.module.css';  
+import styles from "./MainBet.module.css";
 
 export const MainBet = () => {
   return (
-    <div
-     
-      className={styles.mainBet}
-    >
+    <div className={styles.mainBet}>
       <MainHeader />
       <UserLabelHeader />
       <BetComponent betFor="Andar" profit={0.96} stake={0} />
--- src/component/game/common/userBet/jsx/UserLabelListWindow.jsx
+++ src/component/game/common/userBet/jsx/UserLabelListWindow.jsx
@@ -12,6 +12,8 @@ export const UserLabelListWindow = () => {
     { name: "A", odd: "1.5", stake: "100", profit: "150" },
     { name: "B", odd: "2.0", stake: "200", profit: "400" },
     { name: "C", odd: "1.8", stake: "150", profit: "270" },
+    { name: "C", odd: "1.8", stake: "150", profit: "270" },
+    { name: "C", odd: "1.8", stake: "150", profit: "270" },
     { name: "A", odd: "1.5", stake: "500", profit: "130" },
     { name: "B", odd: "2.0", stake: "200", profit: "400" },
   ];
