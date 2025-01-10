3bc28abf55a7ad40c1f4a2ba17192bc8eefea17d
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-01-10 20:28:14 +0530

    feat: Enhance GameInterface to display game name and round ID with improved layout

--- src/component/game/common/layout/jsx/GameInterface.jsx
+++ src/component/game/common/layout/jsx/GameInterface.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../style/GameInterface.module.css";
 import { CardSection as Lucky7BCardSection } from "../../../lucky7B/jsx/CardSection";
 import { CardSection as AndarBaharCardSection } from "../../../AndarBahar/jsx/CardSection";
 
-export const GameInterface = ({ game }) => {
-  let content;
+export const GameInterface = ({ game, roundId = "A85745846" }) => {
+  const [gameName, setGameName] = useState("");
+  const [content, setContent] = useState(null);
 
-  switch (game) {
-    case "lucky7B":
-      content = <Lucky7BCardSection />;
-      break;
+  useEffect(() => {
+    switch (game) {
+      case "lucky7B":
+        setContent(<Lucky7BCardSection />);
+        setGameName("Lucky 7B");
+        break;
 
-    case "andarBahar":
-      content = <AndarBaharCardSection />;
-      break;
+      case "andarBahar":
+        setContent(<AndarBaharCardSection />);
+        setGameName("Andar Bahar");
+        break;
 
-    default:
-      content = <div>Game not found</div>;
-  }
+      default:
+        setGameName("Game not found");
+        setContent(<div>Game not found</div>);
+    }
+  }, [game]);
 
-  return <div className={styles.gameInterface}>{content}</div>;
+  return (
+    <div className={styles.gameInterface}>
+      <div className={styles.header}>
+        <div className={styles.gameDetail}>{gameName}</div>
+        <div className={styles.gameRoundId}>{roundId}</div>
+      </div>
+      <div className={styles.content}>{content}</div>
+    </div>
+  );
 };
