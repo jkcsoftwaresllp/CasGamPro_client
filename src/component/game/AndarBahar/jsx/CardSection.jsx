55c39c17ba3e79e52b57746c15feb63dcad71568
Danishan <danishan.farookh@jkcsoftware.com>
2025-01-11 13:56:39 +0530

    Feat: integrate socket functionality for Andar Bahar game state updates

--- src/component/game/AndarBahar/jsx/CardSection.jsx
+++ src/component/game/AndarBahar/jsx/CardSection.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../style/CardSection.module.css";
 import { CardSection as ABCardSection } from "../../common/cardSection/jsx/CardSection";
+import {
+  connectSocket,
+  disconnectSocket,
+  subscribeToEvent,
+  emitEvent,
+} from "../../services/socketService";
+import { GAME_TYPES } from "../../helper/gameTypes";
 
-export const CardSection = ({}) => {
-  const cards = [
-    "D10",
-    "CA",
-    "D5",
-    "HQ",
-    "H4",
-    "H6",
-    "D6",
-    "D8",
-    "H9",
-    "H10",
-    "S2",
-    "S3",
-  ];
+export const CardSection = () => {
+  const [totalCards, setTotalCards] = useState([]);
+
+  useEffect(() => {
+    const socket = connectSocket("game");
+
+    socket.on("connect", () => {
+      emitEvent("joinGameType", GAME_TYPES.ANDAR_BAHAR);
+    });
+
+    subscribeToEvent("gameStateUpdate", (updatedState) => {
+      console.log("Updated State:", updatedState);
+
+      if (updatedState) {
+        const { andarCards, baharCards } = updatedState;
+        const maxLength = Math.max(andarCards.length, baharCards.length);
+        const combinedCards = [];
+
+        for (let i = 0; i < maxLength; i++) {
+          if (i < baharCards.length && baharCards[i] !== null) {
+            combinedCards.push(baharCards[i]);
+          }
+          if (i < andarCards.length && andarCards[i] !== null) {
+            combinedCards.push(andarCards[i]);
+          }
+        }
+
+        setTotalCards(combinedCards);
+      }
+    });
+
+    socket.on("connect_error", (error) => {
+      console.error("Connection error:", error);
+    });
+
+    socket.on("error", (error) => {
+      console.error("Socket error:", error);
+    });
+
+    return () => {
+      disconnectSocket();
+    };
+  }, []);
 
   return (
     <div className={styles.cardSection}>
-      <ABCardSection playerA="Andar" playerB="Bahar" cards={cards} />
+      <ABCardSection playerA="Andar" playerB="Bahar" cards={totalCards} />
     </div>
   );
 };
