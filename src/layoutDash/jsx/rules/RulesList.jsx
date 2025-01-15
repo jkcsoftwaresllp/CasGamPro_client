476e7c486e0312d00d45574aaafe553f2d3d164b
Rishabh Mishra <rishabh.mishra@jkcsoftwares.com>
2025-01-15 18:13:33 +0530

    feat: update rules in RulesList component for clarity and user guidance

--- src/layoutDash/jsx/rules/RulesList.jsx
+++ src/layoutDash/jsx/rules/RulesList.jsx
@@ -3,10 +3,13 @@ import style from "../../style/Rules.module.css";
 
 export const RulesList = ({ language }) => {
   const rules = {
-    hindi: ["नियम 1: कृपया स्वच्छता बनाए रखें।", "नियम 2: समय का पालन करें।"],
+    hindi: [
+      "नियम 1: कृपया नियमों को समझने के लिए यहां कुछ मिनट दें, और अपने अनुसार समझ लें।",
+      "नियम 2: लॉग इन करने के बाद अपना पासवर्ड बदल लें |",
+    ],
     english: [
-      "Rule 1: Please maintain cleanliness.",
-      "Rule 2: Be punctual at all times.",
+      "Rule 1: Please give a few minutes to understand rules here, as best as you can.",
+      "Rule 2: Change your password after you log in.",
     ],
   };
 
