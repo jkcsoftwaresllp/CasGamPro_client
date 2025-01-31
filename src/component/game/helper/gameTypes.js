867ebd4a1db2137358d4b7b6bebd123f0e251dbc
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-01-31 23:53:24 +0530

    Fix: Andar bahar 1 & 2 UI Working Fine

--- src/component/game/helper/gameTypes.js
+++ src/component/game/helper/gameTypes.js
@@ -1,7 +1,6 @@
 export const GAME_TYPES = {
   andarBahar1: "ANDAR_BAHAR",
-  andarBahar2: "ANDAR_BAHAR",
-  andarBahar: "ANDAR_BAHAR",
+  andarBahar2: "ANDAR_BAHAR_TWO",
   dragonTiger: "DRAGON_TIGER",
   lucky7B: "LUCKY7B",
   teenPattiT20: "TEEN_PATTI",
