47a64e4f9fc9609b4c4dc7df088c296f09dd46e9
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-02-01 02:07:37 +0530

    Feat: BetSection Component in Dragon Tiger Completed

--- src/component/game/dragonTiger/jsx/BetSection.jsx
+++ src/component/game/dragonTiger/jsx/BetSection.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import styles from "../style/BetSection.module.css";
 
 import { BetOnBottom } from "./BetOnBottom";
+import { BetOnTop } from "./BetOnTop";
 
 export const BetSection = ({ onClick, isLock }) => {
   return (
     <div className={styles.betSection}>
+      <BetOnTop onClick={onClick} isLock={isLock} />
       <div className={styles.dragonTiger}>
         <BetOnBottom onClick={onClick} isLock={isLock} side={"D"} />
         <BetOnBottom onClick={onClick} isLock={isLock} side={"T"} />
