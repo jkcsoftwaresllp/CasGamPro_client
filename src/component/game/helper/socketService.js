c497c9b70c625231dbe38397aefd96e47d253a25
kinjalkt3 <kinjalk.tripathi@jkcsoftwares.com>
2025-02-24 15:40:37 +0530

    fixed ws

--- src/component/game/helper/socketService.js
+++ src/component/game/helper/socketService.js
@@ -4,30 +4,24 @@ const isDevelopment = import.meta.env.DEV;
 const productionIP = '88.222.214.174';
 
 const URL = isDevelopment
-  ? 'http://localhost:4320'
-  : `http://${productionIP}:4320`;
-
-const socketOptions = {
-  path: '/socket.io/',
-  transports: ['websocket', 'polling'],
-  reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 1000,
-  autoConnect: true,
-  withCredentials: true
-};
+  ? 'http://localhost:4320/'
+  : `http://${productionIP}:4320/`;
 
 const sockets = {};
 
 export const connectSocket = (namespace) => {
   const url = URL + namespace;
 
+  console.log("thsi", url);
+
+  // If the socket for this namespace does not exist or is disconnected, create a new one
   if (!sockets[namespace] || !sockets[namespace].connected) {
-    sockets[namespace] = io(url, socketOptions);
+    sockets[namespace] = io(url);
   }
 
   return sockets[namespace];
 };
+
 
 export const disconnectSocket = (namespace) => {
   if (sockets[namespace]) {
