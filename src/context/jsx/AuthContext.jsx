36c1920f1715a0dfddd7997a750593e279410389
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-03-01 12:20:49 +0530

    Feat: enhance logout functionality to include navigation after logout

--- src/context/jsx/AuthContext.jsx
+++ src/context/jsx/AuthContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState, useEffect } from "react";
 import { login } from "../helper/login";
 import { logout } from "../helper/logout";
 import { checkSession } from "../helper/checkSession";
+import { useNavigate } from "react-router-dom";
 
 // Create AuthContext
 const AuthContext = createContext();
@@ -9,9 +10,10 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate()
 
   const handleLogin = async (credentials) => await login(setUser, credentials);
-  const handleLogout = async () => logout(setUser);
+  const handleLogout = async () => logout(setUser, navigate);
 
   // Check session on mount
   useEffect(() => {
