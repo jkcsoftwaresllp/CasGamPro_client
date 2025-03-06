f3e6c9f08714aac5c8f91372ff4a35d60142ae6f
Mohd Salman <mohd.salman@jkcsoftwares.com>
2025-03-06 22:04:02 +0530

    Refactor Tab component for improved icon rendering and update styles for better layout and responsiveness in AgentSidebar

--- src/component/agent/main/jsx/Tab.jsx
+++ src/component/agent/main/jsx/Tab.jsx
@@ -6,9 +6,7 @@ export const Tab = ({ icon, onClick, title, isMinimized }) => {
       className={style.tab}
       onClick={onClick} // Triggered on click
     >
-      <div>
-        {typeof icon === "string" ? <img src={icon} alt={title} /> : icon}
-      </div>
+      <div>{icon}</div>
       {!isMinimized && <span>{title}</span>}
     </div>
   );
