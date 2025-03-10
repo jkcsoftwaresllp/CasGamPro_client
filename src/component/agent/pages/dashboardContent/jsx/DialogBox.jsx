bafda047ad63de164b3cd98456f8a61d08c79f49
Danishan Farookh <danishan.farookh@jkcsoftwares.com>
2025-03-10 13:25:34 +0530

    Fix: API call in DialogBox component to use common apiCall function and handle response codes

--- src/component/agent/pages/dashboardContent/jsx/DialogBox.jsx
+++ src/component/agent/pages/dashboardContent/jsx/DialogBox.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { TableOverWindow } from "../TableOverWindow.jsx";
+import { apiCall } from "../../../../common/apiCall.js";
 
 export const DialogBox = ({ isOpen, onClose, header, clientId }) => {
   const [tableData, setTableData] = useState([]);
@@ -14,17 +15,17 @@ export const DialogBox = ({ isOpen, onClose, header, clientId }) => {
   const fetchClientData = async (clientId) => {
     setLoading(true); // Start loading before the API call
     try {
-      const response = await fetch(`/agent/exposure/${clientId}`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch exposure data");
+      const response = await apiCall(
+        `/auth-api/agent/exposure/${clientId}`,
+        "GET"
+      );
+      console.log(response);
+      if (
+        response.uniqueCode === "CGP0059" ||
+        response.uniqueCode === "CGP0060"
+      ) {
+        setTableData(response.data);
       }
-      const data = await response.json();
-      const formattedData = data.map((item) => ({
-        matchName: item.matchName, // Assuming the API returns matchName, marketFancyName, and exposure
-        marketFancyName: item.marketFancyName,
-        exposure: item.exposure,
-      }));
-      setTableData(formattedData);
     } catch (error) {
       console.error("Error fetching client data:", error);
       setTableData([]);
